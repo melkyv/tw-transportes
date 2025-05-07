f1c96ec4cf5986fa5a232873a62cbe2b862607fc
melkyv <melkytam17@gmail.com>
2025-05-06 21:56:52 -0300

    Adiciona migrations, models e páginas iniciais

---
+++ app/Http/Controllers/RastreamentoController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Frete;
+use Illuminate\Http\Request;
+
+class RastreamentoController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $codigoRastreio = $request->input('codigo_rastreio', '');
+
+        $frete = Frete::where('codigo_rastreio', $codigoRastreio)
+            ->with('etapas')
+            ->first();
+
+        if (!$frete) {
+            return back()->with('error', 'Frete não encontrado');
+        }
+        
+        return view('frete.rastreamento', compact('frete'));
+    }
+}
