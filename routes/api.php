9d57488bcf151af2bf4c293374882be9e3b9af2f
melkyv <melkytam17@gmail.com>
2025-05-09 22:15:36 -0300

    Implementa endpoint de cadastro de etapas na api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\EtapaController;
 use App\Http\Controllers\FreteController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,5 @@ Route::get('/up', fn () => ['status' => 'OK']);
 Route::post('/clientes', [ClienteController::class, 'store']);
 
 Route::post('/fretes', [FreteController::class, 'store']);
+
+Route::post('/etapas', [EtapaController::class, 'store']);
